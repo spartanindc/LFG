de23f71a95cc1a38b12de0a68d9c4d4653eeafbf
David Alim <daveay@gmail.com>
2019-01-03 20:22:27 -0500

    Schema and Serialization for Game model

--- api/models/game.js
+++ api/models/game.js
@@ -1,4 +1,29 @@
 // load the things we need
 const mongoose = require("mongoose");
 
-const gameSchema = mongoose.Schema({});
+const gameSchema = mongoose.Schema({
+  gameTitle: String,
+  minPlayers: Number,
+  maxPlayers: Number,
+  description: String,
+  complexity: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+});
+
+gameSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    gameTitle: this.gameTitle,
+    players: `${this.minPlayers} to ${this.maxPlayers}`,
+    description: this.description,
+    complexity: this.complexity,
+    user: this.user.username
+  };
+};
+
+const Game = mongoose.model('Game', gameSchema);
+
+module.exports = { Game };
