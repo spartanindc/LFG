ec2c2369a133664153f4224f3965908262b9c3b6
Unknown <daveay@gmail.com>
2019-01-24 19:52:06 -0500

    back to react only

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -1,8 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Provider } from "react-redux";
-import { ConnectedRouter } from "connected-react-router";
-import configureStore, { history } from "../store";
 
 import App from "./App";
 import Signup from "./Signup";
@@ -10,22 +7,16 @@ import Login from "./Login";
 import Dashboard from "./Dashboard";
 import NotFound from "./NotFound";
 
-const store = configureStore();
-
 const Router = () => (
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={App} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/login" component={Login} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
-    </ConnectedRouter>
-  </Provider>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/login" component={Login} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Router;
